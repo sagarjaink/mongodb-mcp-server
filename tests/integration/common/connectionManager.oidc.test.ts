fe38463ccb5cbd8b402f72722bf0948e96b9eb05
Kevin Mas Ruiz <kevin.mas@hey.com>
2025-10-24 12:30:30 +0200

    chore: retry the whole test with fixtures in case of error (#684)

--- tests/integration/common/connectionManager.oidc.test.ts
+++ tests/integration/common/connectionManager.oidc.test.ts
@@ -13,7 +13,8 @@ import type { OIDCMockProviderConfig } from "@mongodb-js/oidc-mock-provider";
 import { OIDCMockProvider } from "@mongodb-js/oidc-mock-provider";
 import { type TestConnectionManager } from "../../utils/index.js";
 
-const DEFAULT_TIMEOUT = 30_000;
+const DEFAULT_TIMEOUT = 60_000;
+const DEFAULT_RETRIES = 5;
 
 // OIDC is only supported on Linux servers
 describe.skipIf(process.platform !== "linux")("ConnectionManager OIDC Tests", async () => {
@@ -107,13 +108,15 @@ describe.skipIf(process.platform !== "linux")("ConnectionManager OIDC Tests", as
             (integration) => {
                 function oidcIt(name: string, cb: Parameters<OidcIt>[1]): void {
                     /* eslint-disable vitest/expect-expect */
-                    it(name, { timeout: DEFAULT_TIMEOUT }, async (context) => {
+                    it(name, { timeout: DEFAULT_TIMEOUT, retry: DEFAULT_RETRIES }, async (context) => {
                         context.skip(
                             await isCommunityServer(integration),
                             "OIDC is not supported in MongoDB Community"
                         );
                         context.skip(
-                            semver.satisfies(await getServerVersion(integration), "< 7", { includePrerelease: true }),
+                            semver.satisfies(await getServerVersion(integration), "< 7", {
+                                includePrerelease: true,
+                            }),
                             "OIDC is only supported on MongoDB newer than 7.0"
                         );
 
